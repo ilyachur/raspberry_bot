4a7cd6ff5114189b1c2af9741e00aca9f2834fb2
Ilya Churaev <ilya.churaev@intel.com>
2015-01-10 00:39:48 +0300

    Added status commands

--- common/commands.py
+++ common/commands.py
@@ -1,4 +1,30 @@
+from common import routines
+
 def get_status(src_list):
+    line = ''
+    ret_info = routines.shell("/opt/vc/bin/vcgencmd measure_temp")
+    line += ret_info[1].decode("utf-8") + '\n'
+    ret_info = routines.shell("/opt/vc/bin/vcgencmd pm_get_status")
+    for param in ret_info[1].decode("utf-8").split(' '):
+        line += param + '\n'
+    ret_info = routines.shell("df -h")
+    line += ret_info[1].decode("utf-8") + '\n'
+
+
+    ret_info = routines.shell("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq")
+    line += "CPU_FREQ_MIN=" + ret_info[1].decode("utf-8") + "KHz" + '\n'
+    ret_info = routines.shell("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq")
+    line += "CPU_FREQ_MAX=" + ret_info[1].decode("utf-8") + "KHz" + '\n'
+    ret_info = routines.shell("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq")
+    line += "CPU_FREQ_CUR=" + ret_info[1].decode("utf-8") + "KHz" + '\n'
+
+
+    ret_info = routines.shell("free -o -h")
+    line += ret_info[1].decode("utf-8") + '\n'
+
+    ret_info = routines.shell("/opt/vc/bin/vcgencmd get_mem gpu  | sed 's/[A-Za-z]*//g' | cut -c 2-")
+    line += "GPU_MEM=" + ret_info[1].decode("utf-8") + "Mb" + '\n'
+
     line = """Status: good
 Work: Normal
 Temperature: 34 Grad"""
--- daemon.py
+++ daemon.py
@@ -51,11 +51,11 @@ if __name__=='__main__':
         exit('Error config file cannot be found!!!')
 
     # Processor temperature statistic thread
-    ptd = CommandDaemon(command='/opt/vc/bin/vcgencmd measure_temp', timeout=300,db_name='./site/raspberry_site/db/db.sqlite3', db_function=function_result)
-    log.info('Running processor temperature daemon...')
-    ptd.setName('proc_temp')
-    ptd.start()
-    print('Running processor temperature daemon...')
+    #ptd = CommandDaemon(command='/opt/vc/bin/vcgencmd measure_temp', timeout=300,db_name='./site/raspberry_site/db/db.sqlite3', db_function=function_result)
+    #log.info('Running processor temperature daemon...')
+    #ptd.setName('proc_temp')
+    #ptd.start()
+    #print('Running processor temperature daemon...')
 
     # Gmail client
     gmw = GMailWorker(username=cfg['bot_gmail_username'], password=cfg['bot_gmail_password'])
